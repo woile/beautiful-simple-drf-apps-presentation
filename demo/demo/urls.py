168eb686ead3063dbc105ba13911161c3a59fa70
Santiago Fraire Willemoës <santiwilly@gmail.com>
2019-04-03 16:51:08 +0200

    fix: added missing image

--- demo/demo/urls.py
+++ demo/demo/urls.py
@@ -7,9 +7,10 @@ from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
 
+TITILE = "Magic app"
 schema_view = get_schema_view(
    openapi.Info(
-      title="PYSS 2018",
+      title=TITILE,
       default_version='v1',
       description="Test description",
    ),
@@ -23,7 +24,8 @@ router.register(r'customers', CustomerViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('docs/', include_docs_urls(title='PYSS 2018')),
+    path('docs/', include_docs_urls(title=TITILE)),
     re_path(r'^schema(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     re_path(r'^openapi/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='redoc-swagger-ui'),
 ] + router.urls
